5710e1815af94829f1bebbabe391d6f27eb4d727
Benoit Balcon <benoit.b.balcon@renault.com>
2021-09-23 17:36:35 +0200

    optimizing Client/Server development workflow

--- index.js
+++ index.js
@@ -74,18 +74,18 @@ app.get("/callback", (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        const { refresh_token } = response.data;
+        const { access_token, refresh_token } = response.data;
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+        });
 
-        axios
-          .get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
-          .then((response) => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch((error) => {
-            res.send(error);
-          });
+        // redirect to react app
+        res.redirect(`http://localhost:3000/?${queryParams}`);
+
+        // pass along tokens in query params
       } else {
-        res.send(response);
+        res.redirect(`/?${querystring.stringify({ error: "invalid token" })}`);
       }
     })
     .catch((error) => {
