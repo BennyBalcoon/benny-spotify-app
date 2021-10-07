35d7a7e6cb2e43337167d6fa1cfe0eb5c775c969
Benoit Balcon <benoit.b.balcon@renault.com>
2021-10-07 18:00:45 +0200

    file to export styles or components

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,2 +1,5 @@
 export { default as GlobalStyle } from "./GlobalStyle";
 export { default as StyleHeader } from "./StyleHeader";
+export { default as StyledGrid } from "./StyledGrid";
+export { default as StyledSection } from "./StyledSection";
+export { default as StyledTrackList } from "./StyledTrackList";
